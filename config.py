507898a38564b01d3dc0466f8e65fa7d672fde24
AnonOby <anonoby2626@outlook.com>
2026-02-14 06:59:12 +0800

    updated config.py

--- config.py
+++ config.py
@@ -5,3 +5,20 @@ from dotenv import  load_dotenv
 # Load environment variables from .env file if it exists
 load_dotenv()
 
+# --------------------------------------------------
+# 1. Path Configuration
+# --------------------------------------------------
+# Since config.py is in the root directory, .parent gives us the root path.
+BASE_DIR = Path(__file__).resolve().parent
+
+# Define the  main data directory
+DATA_DIR = BASE_DIR / "data"
+
+# Define the subdirectories for raw and processed data
+RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
+
+# Create these directories if they don't exist yet
+RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
+
