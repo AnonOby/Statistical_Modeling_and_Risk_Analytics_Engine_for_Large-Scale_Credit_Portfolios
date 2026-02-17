92d6b7e2686f5b5e3fef46e2326f86af00225bfa
AnonOby <anonoby2626@outlook.com>
2026-02-18 02:22:36 +0800

    added extractor.py

---
+++ src/etl/extractor.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import sys
+import os
+from tqdm import tqdm
+
+# Add project root to path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+import config
+
