a4449090b420ed55bbe674035f17aa82389accf2
AnonOby <anonoby2626@outlook.com>
2026-02-18 10:44:38 +0800

    updated explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -5,3 +5,7 @@ import os
 # Add project root to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
+# import our previously built extractor and config
+from src.etl.extractor import DataExtractor
+import config
+
