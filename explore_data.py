ca5c9331e2cc4c478b660d424859355c75ac9293
AnonOby <anonoby2626@outlook.com>
2026-02-18 12:07:24 +0800

    updated explore_data.py

--- explore_data.py
+++ explore_data.py
@@ -2,6 +2,8 @@ import pandas as pd
 import sys
 import os
 
+from fontTools.merge.util import first
+
 # Add project root to path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
@@ -13,6 +15,15 @@ def main():
     print("Initializing Data Exploration...")
     print("-" * 50)
 
-    # Initialize Extractor
+    # 1. Initialize Extractor
     extractor = DataExtractor(config.RAW_LOAN_DATA_FILE)
 
+    try:
+        # 2. Read only the first small chunk (e.g. 10,000 rows) to peek
+        print("reading the first 10,000 rows from the CSV...")
+
+        # Get just the first yield
+        first_chunk = next(extractor.get_chunks(chunksize=10000))
+
+        print(f"Successfully loaded {first_chunk.shape[0]} rows.")
+        print(f"Total Columns in Dataset: {first_chunk.shape[1]}")
